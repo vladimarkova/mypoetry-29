97d54dfba7b9406ee29a61d6502e3a374875e1ff
vladimarkova <vladislavam1996@gmail.com>
2023-10-25 22:08:10 +0300

    wip.

---
+++ src/app/+store/models/poetry.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+// import { Connect } from "ngrx-action-bundles";
+// import { listingListBundles } from "@app/listing/+store/bundles/listing-list";
+// import { listingListSelectors } from "@app/listing/+store/selectors";
+
+@Injectable()
+
+export class ListingListModel {
+  // actions = this.connect.connectBundles(listingListBundles);
+  // selectors = this.connect.connectSelectors(listingListSelectors);
+  // constructor(private connect: Connect) {}
+}
